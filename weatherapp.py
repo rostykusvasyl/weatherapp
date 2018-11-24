cd11c8f93bb77287ac9158b6b28892744bafe0cc
rostykusvasyl <rostykusvasyl@gmail.com>
2018-11-25 01:42:33 +0200

    Initial commit.

--- weatherapp.py
+++ weatherapp.py
@@ -8,7 +8,10 @@ accu_tags = ('<span class="large-temp">', '<span class="cond">')
 rp5_url = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%91%D1%80%D0%BE%D0%B4%D0%B0%D1%85,'
             '_%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C')
 rp5_tags = ('<span class="t_0" style="display: block;">',
-            '<b>Сьогодні очікується <span class="t_0">')
+            '<div class="cn6" onmouseover="tooltip(this, \'<b>')
+
+container_tags = '<td colspan="2" class="n"> <div class="cc_0">'
+
 
 # url = ('https://ua.sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%B1%D1%80%D0%BE%D0%B4%D0%B8')
 # args = ('<p class="today-temp">', '<div class="weatherIco d400" title=')
@@ -29,7 +32,9 @@ def get_weather_info(get_page, tags):
     def get_tag_content(get_page, tag):
         '''Отримуємо інформацію із веб сторінки про стан погоди для заданого тегу
         '''
-        tag_index = get_page.find(tag)
+        container_tags = '<div id="forecastShort-content">'
+        tag_index = get_page.find(tag, get_page.find(container_tags))
+        print(tag_index)
         tag_size = len(tag)
         value_start = tag_index + tag_size
         content = ''
