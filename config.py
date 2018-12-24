6c272480f4d58f3243dab93135f6cc1ab2afb62a
rostykusvasyl <rostykusvasyl@gmail.com>
2018-12-24 22:23:26 +0200

    [200~Aborting commit due to empty commit message.

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 """ Module contains constants (default values).
 """
 
+# AccuWeather provider reloaded configuration.
 ACCU_PROVIDER_NAME = 'accu'
+ACCU_PRVIDER_TITLE = 'AccuWeather'
 
 # The name of the city and the address for which we will display the weather
 # conditions by default for site accuweather.
-DEFAULT_NAME = 'Brody'
-DEFAULT_URL = ('https://www.accuweather.com/uk/ua/'
-               'brody/324506/weather-forecast/324506')
+DEFAULT_ACCU_LOCATION_NAME = 'Brody'
+DEFAULT_ACCU_LOCATION_URL = ('https://www.accuweather.com/uk/ua/'
+                             'brody/324506/weather-forecast/324506')
 
 # Address for finding a place for which we will display weather conditions.
 ACCU_BROWSE_LOCATIONS = 'https://www.accuweather.com/uk/browse-locations'
@@ -25,14 +27,16 @@ CACHE_TIME = 300
 
 
 RP5_PROVIDER_NAME = 'rp5'
+RP5_PROVIDER_TITLE = 'rp5.ua'
 
 # The name of the city and the address for which we will display the weather
 # conditions by default for site rp5.
-DEFAULT_NAME_RP5 = 'Brody'
-DEFAULT_URL_RP5 = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%'
-                   'B2_%D0%91%D1%80%D0%BE%D0%B4%D0%B0%D1%85,_%D0%9B%D1%8C%'
-                   'D0%B2%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%'
-                   'B0_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C')
+DEFAULT_RP5_LOCATION_NAME = 'Brody'
+DEFAULT_RP5_LOCATION_URL = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B'
+                            '4%D0%B0_%D0%B2_%D0%91%D1%80%D0%BE%D0%B4%D0%B'
+                            '0%D1%85,_%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%'
+                            'D1%8C%D0%BA%D0%B0_%D0%BE%D0%B1%D0%BB%D0%B0%D1%'
+                            '81%D1%82%D1%8C')
 
 # Address for finding a place for which we will display weather conditions.
 RP5_BROWSE_LOCATIONS = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_'
@@ -44,11 +48,15 @@ CONFIG_LOCATION = 'Location'
 CONFIG_FILE_RP5 = 'weather_rp5.ini'
 
 
-# SINOPTIK_PROVIDER_NAME = 'sinoptik'
+SINOPTIK_PROVIDER_NAME = 'sinoptik'
+SINOPTIK_PROVIDER_TITLE = 'sinoptik.ua'
 
-# DEFAULT_NAME_SINOPTIK = 'Brody'
-# DEFAULT_URL_SINOPTIK = ('https://ua.sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B'
-#                         '4%D0%B0-%D0%B1%D1%80%D0%BE%D0%B4%D0%B8')
+DEFAULT_SINOPTIK_LOCATION_NAME = 'Brody'
+DEFAULT_SINOPTIK_LOCATION_URL = ('https://ua.sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B'
+                        '4%D0%B0-%D0%B1%D1%80%D0%BE%D0%B4%D0%B8')
 
-# SINOPTIK_BROWSE_LOCATIONS = ('https://ua.sinoptik.ua/%D1%83%D0%BA%D1%80%D'
-#                              '0%B0%D1%97%D0%BD%D0%B0')
+SINOPTIK_BROWSE_LOCATIONS = ('https://ua.sinoptik.ua/%D1%83%D0%BA%D1%80%D'
+                             '0%B0%D1%97%D0%BD%D0%B0')
+
+CONFIG_LOCATION = 'Location'
+CONFIG_FILE_SINOPTIK = 'sinoptik.ini'
