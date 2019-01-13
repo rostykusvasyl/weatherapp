1fdde809f41a43d7874ac700dbdbc8d4b055ea55
rostykusvasyl <rostykusvasyl@gmail.com>
2019-01-13 15:36:03 +0200

    Fix provider manager.
    	- use command manager as base class
    	- remove all unnecessary code

--- providermanager.py
+++ providermanager.py
@@ -3,38 +3,16 @@
 
 from providers import AccuProvider, Rp5Provider, SinoptikProvider
 
+import commandmanager
 
-class ProviderManager(object):
+
+class ProviderManager(commandmanager.CommandManager):
     """ Discovers registered providers and loads them.
     """
 
-    def __init__(self):
-        self._providers = {}
-        self._load_providers()
-
-    def _load_providers(self):
+    def _load_commands(self):
         """ Loads all existing providers.
         """
 
         for provider in [AccuProvider, Rp5Provider, SinoptikProvider]:
             self.add(provider.name, provider)
-
-    def add(self, name, provider):
-        """ Add new provider by name.
-        """
-
-        self._providers[name] = provider
-
-    def get(self, name):
-        """ Get provider by name.
-        """
-        return self._providers.get(name, None)
-
-    def __len__(self):
-        return len(self._providers)
-
-    def __contains__(self, name):
-        return name in self._providers
-
-    def __getitem__(self, name):
-        return self._providers[name]
