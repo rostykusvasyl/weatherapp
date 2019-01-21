42a3fc8e7fb67a0e9b063ac39c3d9dd53c90be1b
rostykusvasyl <rostykusvasyl@gmail.com>
2019-01-21 12:11:10 +0200

    Split abstract to separate modules.

---
+++ weatherapp/core/abstract/__init__.py
@@ -0,0 +1,7 @@
+from weatherapp.core.abstract.command import Command
+from weatherapp.core.abstract.manager import Manager
+from weatherapp.core.abstract.provider import WeatherProvider
+# from weatherapp.core.abstract.formatter import Formatter
+
+
+__all__ = ['Command', 'Manager', 'WeatherProvider', 'Formatter']
