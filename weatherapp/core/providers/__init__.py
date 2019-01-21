b52a0b21add30590933ae4a5ab3e7d644bd069ea
rostykusvasyl <rostykusvasyl@gmail.com>
2019-01-21 12:08:00 +0200

    Split providers module to separate modules.

---
+++ weatherapp/core/providers/__init__.py
@@ -0,0 +1,3 @@
+from weatherapp.core.providers.accuprovider import AccuProvider
+from weatherapp.core.providers.rp5provider import Rp5Provider
+from weatherapp.core.providers.sinoptikprovider import SinoptikProvider
