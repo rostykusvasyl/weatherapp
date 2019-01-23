18403d55ef23dc39db0750912ba467ca64460c71
rostykusvasyl <rostykusvasyl@gmail.com>
2019-01-23 13:57:32 +0200

    Add abstract class for formatters.

--- weatherapp/core/abstract/__init__.py
+++ weatherapp/core/abstract/__init__.py
@@ -1,7 +1,7 @@
 from weatherapp.core.abstract.command import Command
 from weatherapp.core.abstract.manager import Manager
 from weatherapp.core.abstract.provider import WeatherProvider
-# from weatherapp.core.abstract.formatter import Formatter
+from weatherapp.core.abstract.formatter import Formatter
 
 
 __all__ = ['Command', 'Manager', 'WeatherProvider', 'Formatter']
