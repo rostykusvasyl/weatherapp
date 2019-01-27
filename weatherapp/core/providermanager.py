e69ce66fbdccc2c33260aef657f1db9f36cb74b0
rostykusvasyl <rostykusvasyl@gmail.com>
2019-01-27 18:03:37 +0200

    Fix provider manager.

--- weatherapp/core/providermanager.py
+++ weatherapp/core/providermanager.py
@@ -1,6 +1,6 @@
 """ Module container for providers.
 """
-
+import logging
 from weatherapp.core.providers import accuprovider, rp5provider,\
     sinoptikprovider
 from weatherapp.core import commandmanager
@@ -9,6 +9,8 @@ from weatherapp.core import commandmanager
 class ProviderManager(commandmanager.CommandManager):
     """ Discovers registered providers and loads them.
     """
+
+    logger = logging.getLogger(__name__)
 
     def _load_commands(self):
         """ Loads all existing providers.
