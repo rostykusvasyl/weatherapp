72aac28e4f08bb538995850973aceb9e50a94c3f
rostykusvasyl <rostykusvasyl@gmail.com>
2019-01-27 17:39:39 +0200

    Import a module to write data to a file (.csv).

--- weatherapp/core/commands/__init__.py
+++ weatherapp/core/commands/__init__.py
@@ -1,3 +1,4 @@
 from weatherapp.core.commands.config import Configurate
 from weatherapp.core.commands.providers import Providers
 from weatherapp.core.commands.clear_cache import ClearCache
+from weatherapp.core.commands.csv_write import CsvWrite
