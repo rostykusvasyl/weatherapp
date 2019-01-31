97a92498ece0d7b7d953167be08296852d7d5e87
rostykusvasyl <rostykusvasyl@gmail.com>
2019-01-31 18:32:32 +0200

    Make command manager iterable.

--- weatherapp/core/providers/__init__.py
+++
@@ -1,3 +0,0 @@
-from weatherapp.core.providers.accuprovider import AccuProvider
-from weatherapp.core.providers.rp5provider import Rp5Provider
-from weatherapp.core.providers.sinoptikprovider import SinoptikProvider
