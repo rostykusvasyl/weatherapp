38c91bad709e3b5619684bf98ec9523be995e22a
rostykusvasyl <rostykusvasyl@gmail.com>
2019-02-20 18:35:36 +0200

    deleted comment

--- weatherapp/core/providermanager.py
+++ weatherapp/core/providermanager.py
@@ -18,9 +18,6 @@ class ProviderManager(commandmanager.CommandManager):
         """ Loads all existing providers.
         """
 
-        # for provider in [accuprovider.AccuProvider, rp5provider.Rp5Provider,
-        #                  sinoptikprovider.SinoptikProvider]:
-        #     self.add(provider.name, provider)
         entry_points = pkg_resources.iter_entry_points(
             config.PROVIDER_EP_NAMESPACE)
         for entry_point in entry_points:
